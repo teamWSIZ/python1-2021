7cdae346fb077b6bf13d847374365ec43d45b2db
Piotr Marecki <pmarecki.pro@gmail.com>
2021-12-18 14:02:21 +0100

    czyszczenie

--- zajecia10/simple/simple.py
+++ zajecia10/simple/simple.py
@@ -9,10 +9,7 @@ After Kivy instantiates a subclass of App, it implicitly searches for a .kv
 file. The file test.kv is selected because the name of the subclass of App is
 TestApp, which implies that kivy should try to load "test.kv". That file
 contains a root Widget.
-<<<<<<< HEAD:zajecia10/simple/simple.py
 """
-=======
-'''
 
 import kivy
 # import os
@@ -20,7 +17,6 @@ import kivy
 
 kivy.require('1.0.7')
 
->>>>>>> ddf80cd9d6f659743465c31c8ed2f93726911588:zajecia10/with_kv/app_with_kv.py
 from kivy.app import App
 
 
--- zajecia9_PIL/a.py
+++ zajecia9_PIL/a.py
@@ -5,23 +5,12 @@ import io
 
 # pip install Pillow
 
-image = Image.open('sample.png')
+image = Image.open('pic.jpg')
 print(image.size)  # (800, 701)
-image = image.crop((0, 0, 600, 600))
+# image = image.crop((0, 0, 600, 600))
 # image = image.resize((100, 100), resample=BICUBIC)
 # image = image.resize((100, 100), resample=BICUBIC)
-image = ImageOps.scale(image, 0.3, resample=BICUBIC)
+# image = ImageOps.scale(image, 0.3, resample=BICUBIC)
 
-# zapis obrazka PIL.Image do tablicy bajtów, czyli "bytes"
-image_bytes = io.BytesIO()
-image.save(image_bytes, format='PNG')
-bytes = image_bytes.getvalue()
-
-# odczyt obrazka PIL.Image z tablicy bajtów, czyli "bytes"
-image2 = Image.open(io.BytesIO(bytes))
-image2.show()
-
-
-
-image.save('sample_th.jpg')
+# image.save('sample_th.jpg')
 image.show()
