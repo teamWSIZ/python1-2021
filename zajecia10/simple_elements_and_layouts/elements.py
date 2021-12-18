0d41a65ef566ce51d1cabe4a04b514c4e94b789b
Piotr Marecki <pmarecki.pro@gmail.com>
2021-12-18 20:28:47 +0100

    add tree, and repair styling and binding

--- zajecia10/simple_elements_and_layouts/elements.py
+++ zajecia10/simple_elements_and_layouts/elements.py
@@ -1,16 +1,23 @@
 from kivy.app import App
+from kivy.core.text import Label
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 
 from PIL import Image
-import os
-os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 
-class Root(BoxLayout):
+
+# todo: windows on VM hack (via Mesa3D)
+# import os
+# os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
+
+class MainWidget(BoxLayout):
+    # class of the root widget
     pass
 
-class Second(BoxLayout):
-    text_ouput = ObjectProperty(None)
+
+class BottomMenu(BoxLayout):
+    xxx = ObjectProperty(None)
+    input_ = ObjectProperty(None)
 
     def print_something(self):
         print('OK')
@@ -20,14 +27,16 @@ class Second(BoxLayout):
 
     def compute_square_from_input(self, text):
         print(f'w polu napisano: {text}')
-        # x = int(text)
+        x = int(text)
         # print(f'square = {x**2}')
-        print(self.ids)
-        # self.text_ouput.text = f'wynik: {x**2}'
+        # print(self.xxx.text)
+        self.xxx.text = f'wynik: {x**2}'
 
 
+# https://kivy.org/doc/stable/guide/lang.html
 class ElementsApp(App):
-    kv_directory = '.'
+    # główna klasa kivy
+    kv_directory = '.'  # tu szuka elements.kv, MyApp -> my.kv
 
 
 if __name__ == '__main__':
